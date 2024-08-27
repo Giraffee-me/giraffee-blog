a3302edfc3873315f37455bb687f4f94f333587c
sondos-saad <so_saad@yahoo.com>
2024-08-27 20:36:27 +0300

    Contact page

--- src/Components/ContactUs.jsx
+++ src/Components/ContactUs.jsx
@@ -1,9 +1,25 @@
-
+import mailPhoto from '../assets/images/mail-white.png'
+import { BiSolidLeftArrow } from "react-icons/bi";
+import { BiSolidRightArrow } from "react-icons/bi";
 
 function ContactUs() {
   return (
-    <section>
-    ContactUs
+    <section className=" py-[3rem]">
+        <div className="w-smallSize sm:w-mainSize mx-auto">
+            <div className=' mb-[2.5rem] sm:w-[500px] mx-auto'>
+                <img src={mailPhoto} alt="aboutPhoto" className='w-full'/>
+            </div>
+            <div className='bg-white border-dashed border-[2px] border-black px-[1rem] py-[2rem] mt-[2rem]'>
+                <div className="flex justify-center mb-[1rem]">
+                    <div className="flex items-center gap-[.5rem] ">
+                        <BiSolidLeftArrow  className="text-primary text-[1.5rem]"/>
+                        <h2 className="text-[2rem] font-bold ">يسعدني تواصلك معي عبر البريد الإلكتروني </h2>
+                        <BiSolidRightArrow className="text-primary text-[1.5rem]"/>
+                    </div>
+                </div>
+                <p className='text-[1.2rem] text-center'> giraffee.me@gmail.com</p>
+            </div>
+        </div>
     </section>
   )
 }
