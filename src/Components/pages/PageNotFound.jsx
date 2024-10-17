676193df347f8c459bc556f598c520ba2cb2b660
IAttiaDev <iattiadev@gmail.com>
2024-10-17 07:57:21 +0300

    Page not found responsive

--- src/Components/pages/PageNotFound.jsx
+++ src/Components/pages/PageNotFound.jsx
@@ -2,16 +2,16 @@ import { Link } from "react-router-dom";
 
 function PageNotFound() {
   return (
-    <div className="w-mainSize h-96 bg-white bg-cover bg-no-repeat gap-8 flex flex-wrap md:flex-nowrap mx-auto mt-24 border-dashed border-[2px] border-brownG justify-center">
+    <div className="w-1/2 items-center sm:items-stretch sm:w-mainSize sm:h-96 bg-white bg-cover bg-no-repeat gap-8 flex flex-col sm:flex-row sm:flex-nowrap mx-auto mt-24 border-dashed border-[2px] border-brownG justify-center">
       <img
         src="/images/giraffee-wearing-suit.png"
         alt="Giraffe Wearing a Suit"
-        className=""
+        className="w-1/2 sm:w-auto"
       />
-      <div className="w-1/2 text-center my-auto">
-        <h1 className="text-8xl mb-4 font-bold text-brownG">404</h1>
-        <h2 className="text-4xl mb-8 text-brwonG">لا نعلم أين تلك الصفحة!</h2>
-        <button className="bg-primary px-[1rem] text-brownG font-bold py-[.5rem] hover:border-dashed border-[2px] border-transparent hover:border-brownG duration-200">
+      <div className="w-3/4 sm:w-1/2 text-center my-auto">
+        <h1 className="text-4xl sm:text-8xl mb-4 font-bold text-brownG">404</h1>
+        <h2 className="text-xl sm:text-4xl mb-8 text-brwonG">لا نعلم أين تلك الصفحة!</h2>
+        <button className="bg-primary px-2 sm:px-[1rem] text-brownG font-bold py-[.5rem] hover:border-dashed border-[2px] border-transparent hover:border-brownG duration-200 mb-3 sm:mb-0">
           <Link to="/">الصفحة الرئيسية</Link>
         </button>
       </div>
--- src/Components/pages/home/HomeHeader.jsx
+++ src/Components/pages/home/HomeHeader.jsx
@@ -1,16 +1,30 @@
 import Home from "../../../assets/images/Giraffee-Home.png";
 import { FaXTwitter } from "react-icons/fa6";
+import { FaRegFileLines } from "react-icons/fa6";
+
 
 function HomeHeader() {
   return (
     <div className="w-smallSize sm:w-mainSize mx-auto flex flex-col lg:flex-row gap-[1rem] items-center">
         <div className="h-[400px] w-full lg:w-[49%] flex flex-col justify-center items-center">
             <img src={Home} alt="giraffee" className="w-[350px] m-auto"/>
-            <a href="https://x.com/iAattia" target="_blank">
-                <FaXTwitter className="text-white text-3xl mb-3"/>
-            </a>
+            <div className="flex gap-8">
+
+                {/********* Twitter Account *********/}
+
+                <a href="https://x.com/iAattia" target="_blank" title="حسابي على منصة X">
+                    <FaXTwitter className="text-white text-3xl mb-3"/>
+                </a>
+
+                {/********* Download My CV *********/}
+
+                <a href="/files/Mahmoud-CV-2024.pdf" download title="تحميل سيرتي الذاتية">
+                    <FaRegFileLines className="font-black text-3xl text-white"/>
+                </a>
+                
+            </div>
         </div>
-        <div className="w-full lg:w-[49%] text-center">
+        <div className="w-full lg:w-[45%] text-center">
             <h2 className="text-[26px] font-bold text-center mb-[1rem] py-3">مدونة شخصية</h2>
             <h4 className="text-[18px] font-bold leading-9 mb-[1rem] px-3">
                 الكثير منا لا يُجيد الحديث لذلك يكون التـدوين طـوق النجـاة لتفريغ الأفكار المتزاحمة في أدمغتنا، وحتى إن كانت تلك الأفكار بسيطة قد يجدها أحدهم في هذا العالم الشاسع ذات قيمة.
